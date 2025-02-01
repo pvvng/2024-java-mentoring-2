24259dbdcd6718eb76fe8b752c43c56849923c07
맥동우 <t6u80o@naver.com>
2025-02-01 23:49:01 +0900

    feat : controller 정의

---
+++ mission/mission3-lotto/src/main/java/com/lotto/controller/LottoController.java
@@ -0,0 +1,14 @@
+package com.lotto.controller;
+
+import com.lotto.view.InputView;
+import com.lotto.view.OutputView;
+
+public class LottoController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public LottoController(InputView inputView, OutputView outputView){
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+}
