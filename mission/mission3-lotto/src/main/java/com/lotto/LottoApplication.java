5f0f3d437da81ad88801515b352d01701a82b4d1
맥동우 <t6u80o@naver.com>
2025-02-02 01:17:20 +0900

    feat : application 정의

--- mission/mission3-lotto/src/main/java/com/lotto/LottoApplication.java
+++ mission/mission3-lotto/src/main/java/com/lotto/LottoApplication.java
@@ -1,7 +1,17 @@
 package com.lotto;
 
+import com.lotto.controller.LottoController;
+import com.lotto.view.InputView;
+import com.lotto.view.OutputView;
+
 public class LottoApplication {
     public static void main(String[] args) {
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        LottoController lottoController = new LottoController(inputView, outputView);
+
+        lottoController.buyLotto();
 
     }
 }
