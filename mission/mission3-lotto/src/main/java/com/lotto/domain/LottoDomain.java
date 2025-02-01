4a7c18064a6a3704943b320d52d560d5edc1c5af
맥동우 <t6u80o@naver.com>
2025-02-02 00:31:26 +0900

    feat : 로또 티켓 생성 메서드

--- mission/mission3-lotto/src/main/java/com/lotto/domain/LottoDomain.java
+++ mission/mission3-lotto/src/main/java/com/lotto/domain/LottoDomain.java
@@ -2,18 +2,33 @@ package com.lotto.domain;
 
 import com.lotto.domain.exception.UnderMinPriceErrorException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoDomain {
     private final int LOTTO_TICKET_PRICE = 1000;
+    private final int LOTTO_TICKET_LENGTH = 6;
 
-    public int calculateLottoCount(int purchaseAmount){
+    public int calculateLottoTicketCount(int purchaseAmount) {
         return purchaseAmount / LOTTO_TICKET_PRICE;
     }
 
-    private int isUnderMinimumLottoPrice(int num) {
+    public int isUnderMinimumLottoPrice(int num) {
         if (num < LOTTO_TICKET_PRICE) {
             throw new UnderMinPriceErrorException(ErrorMessage.MIN_PRICE_ERROR.getMessage());
         }
 
         return num;
     }
+
+    public LottoTicket buildLottoTicket(LottoNumberGenerator lottoNumberGenerator) {
+        List<Integer> lottoNumbers = new ArrayList<>();
+
+        for (int i = 0; i < LOTTO_TICKET_LENGTH; i++) {
+            lottoNumbers.add(lottoNumberGenerator.getRandomNumber());
+        }
+
+        return new LottoTicket(lottoNumbers);
+    }
+
 }
