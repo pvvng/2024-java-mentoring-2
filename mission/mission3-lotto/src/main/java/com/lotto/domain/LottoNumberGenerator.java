1d0cc1b316356734d614d46e0e84a36afae050ad
맥동우 <t6u80o@naver.com>
2025-02-02 00:20:29 +0900

    feat : 로또 랜덤넘버 생성 클래스

---
+++ mission/mission3-lotto/src/main/java/com/lotto/domain/LottoNumberGenerator.java
@@ -0,0 +1,10 @@
+package com.lotto.domain;
+
+public class LottoNumberGenerator {
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+
+    public int getRandomNumber() {
+        return (int) (Math.random() * MAX_LOTTO_NUMBER) + MIN_LOTTO_NUMBER;
+    }
+}
