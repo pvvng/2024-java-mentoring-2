d4d97bcf25ba44b4038789d3f45f094484b582b0
맥동우 <t6u80o@naver.com>
2025-02-02 00:20:12 +0900

    feat : 구매할 로또 숫자 구하기

--- mission/mission3-lotto/src/main/java/com/lotto/controller/LottoController.java
+++ mission/mission3-lotto/src/main/java/com/lotto/controller/LottoController.java
@@ -1,19 +1,30 @@
 package com.lotto.controller;
 
+import com.lotto.domain.IntegerParser;
+import com.lotto.domain.LottoDomain;
 import com.lotto.view.InputView;
 import com.lotto.view.OutputView;
 
 public class LottoController {
     private final InputView inputView;
     private final OutputView outputView;
+    private final LottoDomain lottoDomain;
+    private final IntegerParser integerParser;
 
     public LottoController(InputView inputView, OutputView outputView){
         this.inputView = inputView;
         this.outputView = outputView;
+        this.lottoDomain = new LottoDomain();
+        this.integerParser = new IntegerParser();
     }
 
     public void buyLotto(){
         outputView.showPurchaseAmount();
-        String userPurchaseAmount = inputView.getInput();
+
+        String stringPurchaseAmount = inputView.getInput();
+
+        int purchaseAmount = integerParser.parseInteger(stringPurchaseAmount);
+        purchaseAmount = lottoDomain.isUnderMinimumLottoPrice(purchaseAmount);
+        int lottoTicketCount = lottoDomain.calculateLottoTicketCount(purchaseAmount);
     }
 }
