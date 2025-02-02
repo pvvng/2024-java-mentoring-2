8cb9d82f1bf37fc5d673fd7f2a3ad46550cba651
맥동우 <t6u80o@naver.com>
2025-02-02 11:57:13 +0900

    feat : service에서 직전 구현한 DTO를 사용하여 컨트롤러와 소통

--- mission/mission3-lotto/src/main/java/com/lotto/service/LottoService.java
+++ mission/mission3-lotto/src/main/java/com/lotto/service/LottoService.java
@@ -1,29 +1,54 @@
 package com.lotto.service;
 
+import com.lotto.domain.IntegerParser;
+import com.lotto.domain.LottoDomain;
 import com.lotto.service.DTO.LottoTicketDTO;
 
 import com.lotto.domain.LottoTicket;
+import com.lotto.service.DTO.RequestLottoTicketCountDTO;
+import com.lotto.service.DTO.ResponseLottoTicketCountDTO;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class LottoService {
     private final LottoTicketFactory lottoTicketFactory;
+    private final IntegerParser integerParser = new IntegerParser();
+    private final LottoDomain lottoDomain = new LottoDomain();
 
-    public LottoService(LottoTicketFactory lottoTicketFactory){
+    public LottoService(LottoTicketFactory lottoTicketFactory) {
         this.lottoTicketFactory = lottoTicketFactory;
     }
 
-    public List<LottoTicketDTO> getLottoTickets(int lottoTicketCount) {
+    public ResponseLottoTicketCountDTO getLottoTicketCountDTO(RequestLottoTicketCountDTO requestDTO) {
+        String stringPurchaseAmount = requestDTO.stringPurchaseAmount();
+
+        int lottoTicketCount = getLottoTicketCount(stringPurchaseAmount);
+
+        return new ResponseLottoTicketCountDTO(lottoTicketCount);
+    }
+
+    public List<LottoTicketDTO> getLottoTicketDTOs(int lottoTicketCount) {
         List<LottoTicketDTO> lottoTicketDTOs = new ArrayList<>();
 
         for (int i = 0; i < lottoTicketCount; i++) {
-            LottoTicket ticket = lottoTicketFactory.getLottoTicket();
-            LottoTicketDTO lottoTicketDTO = new LottoTicketDTO(ticket.lottoNumbers());
-            lottoTicketDTOs.add(lottoTicketDTO);
+            LottoTicket lottoTicket = lottoTicketFactory.getLottoTicket();
+            lottoTicketDTOs.add(createLottoTicketDTO(lottoTicket));
         }
 
         return lottoTicketDTOs;
+    }
+
+    private LottoTicketDTO createLottoTicketDTO(LottoTicket lottoTicket){
+
+        return new LottoTicketDTO(lottoTicket.lottoNumbers());
+    }
+
+    private int getLottoTicketCount(String stringPurchaseAmount) {
+        int purchaseAmount = integerParser.parseInteger(stringPurchaseAmount);
+        int verifiedPurchaseAmount = lottoDomain.isUnderMinimumLottoPrice(purchaseAmount);
+
+        return lottoDomain.calculateLottoTicketCount(verifiedPurchaseAmount);
     }
 
 }
