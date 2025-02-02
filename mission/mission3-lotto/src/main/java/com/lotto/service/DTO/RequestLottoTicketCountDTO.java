57698b0f2f056fa3e5bd1e147bf23dccd214f108
맥동우 <t6u80o@naver.com>
2025-02-02 11:56:39 +0900

    feat : 사용자가 입력한 구매 금액 DTO, 구매 금액으로 구매 가능한 로또 개수를 수송하는 dto

---
+++ mission/mission3-lotto/src/main/java/com/lotto/service/DTO/RequestLottoTicketCountDTO.java
@@ -0,0 +1,4 @@
+package com.lotto.service.DTO;
+
+public record RequestLottoTicketCountDTO(String stringPurchaseAmount) {
+}
---
+++ mission/mission3-lotto/src/main/java/com/lotto/service/DTO/ResponseLottoTicketCountDTO.java
@@ -0,0 +1,4 @@
+package com.lotto.service.DTO;
+
+public record ResponseLottoTicketCountDTO(int purchaseAmount) {
+}
