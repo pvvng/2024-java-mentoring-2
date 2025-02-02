71254976e1a00b690c7ccdb06c0aaa71dc76e860
맥동우 <t6u80o@naver.com>
2025-02-02 11:04:45 +0900

    feat : 도메인에서 서비스로 이동시킨 메서드 제거

--- mission/mission3-lotto/src/main/java/com/lotto/domain/LottoDomain.java
+++ mission/mission3-lotto/src/main/java/com/lotto/domain/LottoDomain.java
@@ -2,14 +2,8 @@ package com.lotto.domain;
 
 import com.lotto.domain.exception.UnderMinPriceErrorException;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 public class LottoDomain {
     private final int LOTTO_TICKET_PRICE = 1000;
-    private final int LOTTO_TICKET_LENGTH = 6;
 
     public int calculateLottoTicketCount(int purchaseAmount) {
         return purchaseAmount / LOTTO_TICKET_PRICE;
@@ -21,37 +15,6 @@ public class LottoDomain {
         }
 
         return num;
-    }
-
-    public String getFormattedLottoTickets(LottoTickets lottoTickets){
-        StringBuilder formatedTickets = new StringBuilder();
-
-        for(LottoTicket lottoTicket : lottoTickets.lottoTickets()){
-            formatedTickets.append(lottoTicket.lottoNumbers()).append("\n");
-        }
-
-        return formatedTickets.toString();
-    }
-
-    public LottoTickets getLottoTickets(LottoNumberGenerator lottoNumberGenerator, int lottoTicketCount) {
-        List<LottoTicket> lottoTickets = new ArrayList<>();
-
-        for (int i = 0; i < lottoTicketCount; i++) {
-            LottoTicket lottoTicket = buildLottoTicket(lottoNumberGenerator);
-            lottoTickets.add(lottoTicket);
-        }
-
-        return new LottoTickets(lottoTickets);
-    }
-
-    private LottoTicket buildLottoTicket(LottoNumberGenerator lottoNumberGenerator) {
-        Set<Integer> lottoNumbers = new HashSet<>();
-
-        while(lottoNumbers.size() != LOTTO_TICKET_LENGTH){
-            lottoNumbers.add(lottoNumberGenerator.getRandomNumber());
-        }
-
-        return new LottoTicket(lottoNumbers);
     }
 
 }
