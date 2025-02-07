41220d455c4fd2227840a32990dd684c10980a9b
맥동우 <t6u80o@naver.com>
2025-02-07 09:59:29 +0900

    feat : controller 의존성 주입

---
+++ mission/mission3-lotto/src/main/java/com/lotto/controller/LottoController.java
@@ -0,0 +1,16 @@
+package com.lotto.controller;
+
+import com.lotto.view.InputView;
+import com.lotto.view.OutputView;
+
+public class LottoController {
+
+    final private InputView inputView;
+    final private OutputView outputView;
+
+    public LottoController(InputView inputView, OutputView outputView){
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+}
