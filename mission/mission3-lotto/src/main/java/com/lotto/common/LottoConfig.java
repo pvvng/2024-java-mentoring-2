0f40cb527761ae15c65734b587cb87fb3a45ee6b
맥동우 <t6u80o@naver.com>
2025-02-07 10:56:48 +0900

    fix : 상수 static final

--- mission/mission3-lotto/src/main/java/com/lotto/common/LottoConfig.java
+++ mission/mission3-lotto/src/main/java/com/lotto/common/LottoConfig.java
@@ -2,9 +2,9 @@ package com.lotto.common;
 
 public class LottoConfig {
 
-    public int MIN_LOTTO_NUMBER = 1;
-    public int MAX_LOTTO_NUMBER = 45;
-    public int LOTTO_PRICE = 1000;
-    public int LOTTO_TICKET_LENGTH = 6;
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
+    public static final int LOTTO_PRICE = 1000;
+    public static final int LOTTO_TICKET_LENGTH = 6;
 
 }
