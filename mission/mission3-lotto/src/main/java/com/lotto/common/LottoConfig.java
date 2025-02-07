aa4edf0c59871b1e7fe038432710642f5b9a4e64
맥동우 <t6u80o@naver.com>
2025-02-07 10:55:04 +0900

    feat : 로또 상수 클래스 정의

---
+++ mission/mission3-lotto/src/main/java/com/lotto/common/LottoConfig.java
@@ -0,0 +1,10 @@
+package com.lotto.common;
+
+public class LottoConfig {
+
+    public int MIN_LOTTO_NUMBER = 1;
+    public int MAX_LOTTO_NUMBER = 45;
+    public int LOTTO_PRICE = 1000;
+    public int LOTTO_TICKET_LENGTH = 6;
+
+}
