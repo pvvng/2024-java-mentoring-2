8d270153a74c44513589e07c3607b3bbe016ac18
맥동우 <t6u80o@naver.com>
2025-02-10 22:33:40 +0900

    feat : 서비스, 로또 번호 분석 결과 받아서 컨트롤러로 전달

--- mission/mission3-lotto/src/main/java/com/lotto/service/LottoService.java
+++ mission/mission3-lotto/src/main/java/com/lotto/service/LottoService.java
@@ -2,6 +2,7 @@ package com.lotto.service;
 
 import com.lotto.domain.LottoTicketsGenerator;
 import com.lotto.domain.WinnerTicketGenerator;
+import com.lotto.domain.LottoWinningStatisticsCalculator;
 import com.lotto.domain.Money;
 import com.lotto.domain.LottoTickets;
 import com.lotto.domain.WinnerTicket;
@@ -11,15 +12,18 @@ import com.lotto.service.DTO.LottoResponseDTO;
 import com.lotto.service.DTO.WinnerRequestDTO;
 
 import java.util.List;
+import java.util.Map;
 
 public class LottoService {
 
     private final LottoTicketsGenerator lottoTicketsGenerator;
     private final WinnerTicketGenerator winnerTicketGenerator;
+    private final LottoWinningStatisticsCalculator statisticsCalculator;
 
-    public LottoService(LottoTicketsGenerator lottoTicketsGenerator, WinnerTicketGenerator winnerTicketGenerator) {
+    public LottoService(LottoTicketsGenerator lottoTicketsGenerator, WinnerTicketGenerator winnerTicketGenerator, LottoWinningStatisticsCalculator statisticsCalculator) {
         this.lottoTicketsGenerator = lottoTicketsGenerator;
         this.winnerTicketGenerator = winnerTicketGenerator;
+        this.statisticsCalculator = statisticsCalculator;
     }
 
     public LottoResponseDTO getLottoResponseDTO(int purchaseAmount){
@@ -34,10 +38,12 @@ public class LottoService {
         );
     }
 
-    public void getWinningStatistics(WinnerRequestDTO requestDTO) {
-        WinnerTicket winnerTicket = winnerTicketGenerator.getWinnerTicket(requestDTO.winnerNumbers());
+    public Map<Integer, Integer> getLottoStatistics(WinnerRequestDTO requestDTO) {
         LottoTickets lottoTickets = requestDTO.lottoTickets();
+        WinnerTicket winnerTicket = winnerTicketGenerator.getWinnerTicket(requestDTO.winnerNumbers());
 
+        Map<Integer, Integer> winningStatistics = statisticsCalculator.calculateWinningStatistics(lottoTickets, winnerTicket);
+        return statisticsCalculator.calculateWinningStatistics(lottoTickets, winnerTicket);
     }
 
     private List<String> convertTicketsToStringList(LottoTickets lottoTickets) {
