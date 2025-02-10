711b937bb28f0acf2a42d300213ff95c6dad51ca
맥동우 <t6u80o@naver.com>
2025-02-10 19:50:01 +0900

    feat : prompt 메서드 정의

--- mission/mission3-lotto/src/main/java/com/lotto/view/OutputView.java
+++ mission/mission3-lotto/src/main/java/com/lotto/view/OutputView.java
@@ -2,16 +2,25 @@ package com.lotto.view;
 
 public class OutputView {
 
-    public void showPurchaseAmount(){
+    public void showPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
-    public void showPurchaseHistory(int purchaseAmount){
+    public void showPurchaseHistory(int purchaseAmount) {
         System.out.println("\n" + purchaseAmount + "개를 구매했습니다.");
     }
 
-    public void showLotto(String lotto){
+    public void showLotto(String lotto) {
         System.out.println(lotto);
+    }
+
+    public void showWinnerNumbersPrompt() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+    }
+
+    public void showStatisticsPrompt() {
+        System.out.println("당첨 통계");
+        System.out.println("---------");
     }
 
 }
