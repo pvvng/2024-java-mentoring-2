4a0922e3ac9a19871d4c2422872616518d515e7b
맥동우 <t6u80o@naver.com>
2025-02-11 13:05:56 +0900

    feat : view 출력 메서드

--- mission/mission3-lotto/src/main/java/com/lotto/view/OutputView.java
+++ mission/mission3-lotto/src/main/java/com/lotto/view/OutputView.java
@@ -1,5 +1,10 @@
 package com.lotto.view;
 
+import com.lotto.common.PrizeMoneyConfig;
+
+import java.util.List;
+import java.util.Map;
+
 public class OutputView {
 
     public void showPurchaseAmount() {
@@ -15,12 +20,23 @@ public class OutputView {
     }
 
     public void showWinnerNumbersPrompt() {
-        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        System.out.println("\n지난 주 당첨 번호를 입력해 주세요.");
     }
 
     public void showStatisticsPrompt() {
-        System.out.println("당첨 통계");
+        System.out.println("\n당첨 통계");
         System.out.println("---------");
+    }
+
+    public void showWinStatistics(Map<Integer, Integer> statisticsMap) {
+        System.out.println("3개 일치 (" + PrizeMoneyConfig.FIFTH_PLACE_PRIZE_MONEY.getPrizeMoney() + ")원- " + statisticsMap.getOrDefault(3, 0) + "개");
+        System.out.println("4개 일치 (" + PrizeMoneyConfig.FOURTH_PLACE_PRIZE_MONEY.getPrizeMoney() + ")원- " + statisticsMap.getOrDefault(4, 0) + "개");
+        System.out.println("5개 일치 (" + PrizeMoneyConfig.THIRD_PLACE_PRIZE_MONEY.getPrizeMoney() + ")원- " + statisticsMap.getOrDefault(5, 0) + "개");
+        System.out.println("6개 일치 (" + PrizeMoneyConfig.FIRST_PLACE_PRIZE_MONEY.getPrizeMoney() + ")원- " + statisticsMap.getOrDefault(6, 0) + "개");
+    }
+
+    public void showROI(String ROI) {
+        System.out.println("총 수익률은 " + ROI + "입니다.");
     }
 
 }
