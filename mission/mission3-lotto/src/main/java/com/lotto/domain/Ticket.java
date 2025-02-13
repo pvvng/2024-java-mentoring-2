1c13c6e6033179719049b266082f43199522e4bb
맥동우 <t6u80o@naver.com>
2025-02-13 14:15:44 +0900

    feat : LottoTicket(자동/수동 티켓), WinnerTicket(당첨 번호) 생성하는 빌더 패턴 Ticket 메서드 작성

--- mission/mission3-lotto/src/main/java/com/lotto/domain/Ticket.java
+++ mission/mission3-lotto/src/main/java/com/lotto/domain/Ticket.java
@@ -4,14 +4,112 @@ import com.lotto.common.ErrorMessage;
 import com.lotto.common.LottoConfig;
 import com.lotto.common.exception.InvalidTicketLength;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-public interface Ticket {
+public class Ticket {
 
-    default void validateTicketLength(List<LottoNumber> lottoTicket) {
-        if (lottoTicket.size() != LottoConfig.LOTTO_TICKET_LENGTH.getConfig()) {
-            throw new InvalidTicketLength(ErrorMessage.INVALID_TICKET_LENGTH_ERROR.getMessage());
+    private final List<LottoNumber> lottoNumbers;
+    private final int bonus;
+    private final TicketType type;
+
+    private Ticket(Builder builder) {
+        this.lottoNumbers = builder.lottoNumbers;
+        this.bonus = builder.bonus;
+        this.type = builder.type;
+    }
+
+    public List<LottoNumber> getLottoNumbers() {
+        return lottoNumbers;
+    }
+
+    public int getBonus() {
+        return bonus;
+    }
+
+    public TicketType getType() {
+        return type;
+    }
+
+    public int getNumber(int index) {
+        return getLottoNumbers().get(index).lottoNumber();
+    }
+
+    public boolean isNumberAt(int index, int number) {
+        return getLottoNumbers().get(index).lottoNumber() == number;
+    }
+
+    @Override
+    public String toString() {
+        return getLottoNumbers().stream()
+                .map(LottoNumber::lottoNumber)
+                .toList()
+                .toString();
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private List<LottoNumber> lottoNumbers;
+        private int bonus = 0;
+        private TicketType type;
+
+        public Builder withRandomNumbers() {
+            this.lottoNumbers = createRandomLottoNumbers();
+            this.type = TicketType.AUTO;
+
+            return this;
+        }
+
+        public Builder withNumbers(List<LottoNumber> numbers) {
+            this.lottoNumbers = numbers;
+            this.type = TicketType.CUSTOM;
+
+            return this;
+        }
+
+        public Builder withBonusNumber(int bonus) {
+            if (type != TicketType.CUSTOM) {
+                throw new UnsupportedOperationException(ErrorMessage.NOT_ALLOWED_TICKET_TYPE.getMessage());
+            }
+
+            this.type = TicketType.WINNER;
+            this.bonus = bonus;
+
+            return this;
+        }
+
+        public Ticket build() {
+            validate();
+
+            return new Ticket(this);
+        }
+
+        private void validate() {
+            if (lottoNumbers == null || lottoNumbers.size() != 6) {
+                throw new InvalidTicketLength(ErrorMessage.INVALID_TICKET_LENGTH_ERROR.getMessage());
+            }
+        }
+
+        private List<LottoNumber> createRandomLottoNumbers() {
+            Set<LottoNumber> ticket = new HashSet<>();
+
+            while (ticket.size() < LottoConfig.LOTTO_TICKET_LENGTH.getConfig()) {
+                ticket.add(new LottoNumber(getRandomNumber()));
+            }
+
+            return ticket.stream().toList();
+        }
+
+        private int getRandomNumber() {
+            return (int) (Math.random() * LottoConfig.MAX_LOTTO_NUMBER.getConfig()) + LottoConfig.MIN_LOTTO_NUMBER.getConfig();
         }
     }
 
+    public enum TicketType {
+        AUTO, CUSTOM, WINNER
+    }
 }
