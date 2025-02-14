d4793066d4bf96173d703495b4eed1adfe75b482
맥동우 <t6u80o@naver.com>
2025-02-14 13:14:40 +0900

    fix : prizeCalculator 리팩토링

--- mission/mission3-lotto/src/main/java/com/lotto/domain/LottoPrizeCalculator.java
+++ mission/mission3-lotto/src/main/java/com/lotto/domain/LottoPrizeCalculator.java
@@ -3,35 +3,23 @@ package com.lotto.domain;
 import com.lotto.common.MatchCountConfig;
 import com.lotto.common.PrizeMoneyConfig;
 
+import java.util.Map;
+
 public class LottoPrizeCalculator {
 
-    public int getPrize(RewardVoucher voucher) {
-        return calculatePrize(voucher.getMatchCount(), voucher.hasBonus());
-    }
+    private static final Map<Integer, Integer> PRIZE_MONEY = Map.of(
+            MatchCountConfig.FIRST_PLACE_MATCH_COUNT.getMatchCount(), PrizeMoneyConfig.FIRST_PLACE_PRIZE_MONEY.getPrizeMoney(),
+            MatchCountConfig.THIRD_PLACE_MATCH_COUNT.getMatchCount(), PrizeMoneyConfig.THIRD_PLACE_PRIZE_MONEY.getPrizeMoney(),
+            MatchCountConfig.FOURTH_PLACE_MATCH_COUNT.getMatchCount(), PrizeMoneyConfig.FOURTH_PLACE_PRIZE_MONEY.getPrizeMoney(),
+            MatchCountConfig.FIFTH_PLACE_MATCH_COUNT.getMatchCount(), PrizeMoneyConfig.FIFTH_PLACE_PRIZE_MONEY.getPrizeMoney()
+    );
 
-    private int calculatePrize(int matchCount, boolean bonus) {
-
-        if (matchCount == MatchCountConfig.FIRST_PLACE_MATCH_COUNT.getMatchCount() && !bonus) {
-            return PrizeMoneyConfig.FIRST_PLACE_PRIZE_MONEY.getPrizeMoney();
-        }
-
+    public static int getPrize(int matchCount, boolean bonus) {
         if (matchCount == MatchCountConfig.SECOND_PLACE_MATCH_COUNT.getMatchCount() && bonus) {
             return PrizeMoneyConfig.SECOND_PLACE_PRIZE_MONEY.getPrizeMoney();
         }
 
-        if (matchCount == MatchCountConfig.THIRD_PLACE_MATCH_COUNT.getMatchCount() && !bonus) {
-            return PrizeMoneyConfig.THIRD_PLACE_PRIZE_MONEY.getPrizeMoney();
-        }
-
-        if (matchCount == MatchCountConfig.FOURTH_PLACE_MATCH_COUNT.getMatchCount()) {
-            return PrizeMoneyConfig.FOURTH_PLACE_PRIZE_MONEY.getPrizeMoney();
-        }
-
-        if (matchCount == MatchCountConfig.FIFTH_PLACE_MATCH_COUNT.getMatchCount()) {
-            return PrizeMoneyConfig.FIFTH_PLACE_PRIZE_MONEY.getPrizeMoney();
-        }
-
-        return 0;
+        return PRIZE_MONEY.getOrDefault(matchCount, 0);
     }
 
 }
